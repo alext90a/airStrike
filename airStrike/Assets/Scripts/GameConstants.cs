38cc945cea6608e8cb3eee9711c2e22b9b18ae9a
alexey.cigelnikov <alexeyt3485@gmail.com>
2017-04-27 13:25:37 +0700

    player movements are added

---
+++ airStrike/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConstants
+{
+
+    public static float kPlayerMoveSpeed = 7f;
+
+    public static float kLeftBorder = -12f;
+    public static float kRightBorder = 12f;
+    public static float kTopBorder = 7f;
+    public static float kBottomBorder = -7f;
+
+}
---
+++ airStrike/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 curPos = transform.position;
+        if(Input.GetKey(KeyCode.A))
+        {
+            curPos.x -= GameConstants.kPlayerMoveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            curPos.x += GameConstants.kPlayerMoveSpeed * Time.deltaTime;
+        }
+
+        if(curPos.x < GameConstants.kLeftBorder)
+        {
+            curPos.x = GameConstants.kLeftBorder;
+        }
+        if(curPos.x > GameConstants.kRightBorder)
+        {
+            curPos.x = GameConstants.kRightBorder;
+        }
+        transform.position = curPos;
+	}
+}
